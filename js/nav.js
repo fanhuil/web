a032fc063193f5c47d35955be09d5aded83994bb
fanhuilin <fanhuilinl@163.com>
2021-04-07 18:06:42 +0800

    no message

--- js/nav.js
+++ js/nav.js
@@ -7,3 +7,19 @@ for (i = 1; i < As.length; i++) {
 		obj = As[i];
 }
 obj.id = 'nava'
+// 第一个wz元素距离顶部的位置
+var wz_top = $(".left").offset().top
+// 头部导航栏元素的高度
+var header = $('#header').height()
+// 头部需要隐藏时滚动条距离顶部的距离
+var hide_header_height = wz_top - header
+// 监听滚动，显示或隐藏头部
+$(document).scroll(function(){
+	var top = $(window).scrollTop()
+	if(top >= hide_header_height){
+		$('#header').fadeOut()
+	}else{
+		$('#header').fadeIn()
+	}
+	
+})
